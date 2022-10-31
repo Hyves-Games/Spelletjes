15342d44ff78029af36d4e9e208dd29d4c83dd7c
Jurrie Piek <jurrie39@gmail.com>
2022-10-31 22:24:47 +0100

    added login on menu

--- src/main/java/client/menu/controller/MenuController.java
+++ src/main/java/client/menu/controller/MenuController.java
@@ -2,13 +2,38 @@ package client.menu.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import support.helpers.SceneSwitcher;
 
-public class MenuController
-{
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class MenuController implements Initializable
+{
+    public Boolean authenticated = false;
     @FXML Label serverStatus;
+    @FXML Button loginBtn;
+    @FXML TextField loginField;
+    @FXML Button playGameBtn;
+    @FXML Button playerListBtn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        if (!authenticated) {
+            playGameBtn.setVisible(false);
+            playerListBtn.setVisible(false);
+        }
+    }
+
+    public void onLoginClick(ActionEvent event) {
+        playGameBtn.setVisible(true);
+        playerListBtn.setVisible(true);
+        loginField.setVisible(false);
+        loginBtn.setVisible(false);
+    }
 
     public void onPlayGameClick(ActionEvent event) {
         SceneSwitcher.switchScene(event, "waitingRoom/waitingroom.fxml", "Waiting room");
