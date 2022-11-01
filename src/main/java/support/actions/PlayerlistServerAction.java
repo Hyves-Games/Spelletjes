09cd3bdb248ab07a514b26657a7d522f7dc94a6a
Jeffrey-Meijer <jeffreymeijer11@gmail.com>
2022-11-01 21:48:38 +0100

    Changed playlist to playerlist

--- src/main/java/support/actions/PlayerlistServerAction.java
+++ src/main/java/support/actions/PlayerlistServerAction.java
@@ -3,13 +3,13 @@ package support.actions;
 import support.abstracts.AbstractServerAction;
 import support.exceptions.NoServerConnectionException;
 
-public class PlaylistServerAction extends AbstractServerAction {
-    public PlaylistServerAction() throws NoServerConnectionException {
+public class PlayerlistServerAction extends AbstractServerAction {
+    public PlayerlistServerAction() throws NoServerConnectionException {
         this.handler();
     }
 
     @Override
     protected void handler() throws NoServerConnectionException {
-        this.command("get playlist");
+        this.command("get playerlist");
     }
 }
