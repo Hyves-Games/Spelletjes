f8c6df829a9af9192b13c2f2f854b13edf8deeaf
Joey Roeters <joeyroeters@outlook.com>
2022-11-02 10:06:54 +0100

    Missing import

--- src/main/java/support/helpers/SceneSwitcher.java
+++ src/main/java/support/helpers/SceneSwitcher.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import support.enums.SceneEnum;
+import support.records.ClientScene;
 
 import java.io.IOException;
 import java.util.HashMap;
