cf7fa51ae99c7cf38ea7653dd6fe2fc69397ed28
Jurrie Piek <jurrie39@gmail.com>
2022-11-02 10:31:49 +0100

    wip

--- src/main/java/client/game/board/controller/TicTacToeController.java
+++ src/main/java/client/game/board/controller/TicTacToeController.java
@@ -1,13 +1,21 @@
 package client.game.board.controller;
 
+import domain.game.model.TicTacToe;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.ProgressBar;
+import support.abstracts.AbstractGameBoard;
 
 public class TicTacToeController {
+
+    @FXML ProgressBar timeLeft;
+
+    private final TicTacToe ticTacToe = new TicTacToe();
+
     public void onMoveClick(ActionEvent event) {
         String index = ((Node)(event.getSource())).getId().split("_")[1];
         System.out.println(index);
-
     }
 
     public void onLeaveClick(ActionEvent event) {
