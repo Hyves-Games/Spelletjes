3192be47771f49e3b6d7aac41c438a63256f5ea4
Xander <a.j.drent@st.hanze.nl>
2022-12-21 21:27:12 +0100

    Added IntArrayToLong

---
+++ src/main/java/domain/game/ai/ReversiAI/Converters/IntArrayToLong.java
@@ -0,0 +1,10 @@
+package domain.game.ai.ReversiAI.Converters;
+
+public class IntArrayToLong {
+    public static long convert(int[] a, int value) {
+        long n = 0;
+        for (int i = 0; i < 64; i++)
+            n = (n << 1) | (a[i] == value ? 1 : 0);
+        return n;
+    }
+}
