d89333e75836e38df7746d95eeedab3dc852bdb0
Xander <a.j.drent@st.hanze.nl>
2023-01-16 12:23:34 +0100

    Fixed converter bug

--- src/main/java/domain/game/ai/ReversiAI/Converters/IntArrayToBoolean.java
+++ src/main/java/domain/game/ai/ReversiAI/Converters/IntArrayToBoolean.java
@@ -20,7 +20,7 @@ public class IntArrayToBoolean {
 
     public static boolean[] convert(int[] moves) {
         //@TODO: merge with other method
-        boolean[] booleanArray = new boolean[moves.length];
+        boolean[] booleanArray = new boolean[64];
         for (int i = 0; i < moves.length; i++) {
             booleanArray[moves[i]] = true;
         }
