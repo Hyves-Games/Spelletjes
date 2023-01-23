e0693f303af9da7c39263c859b9e3c6bdb52a8d3
Jeffrey-Meijer <jeffreymeijer11@gmail.com>
2023-01-23 15:08:18 +0100

    Added MiniMaxAI class

---
+++ src/main/java/domain/game/ai/ReversiAI/AIs/MiniMaxAI.java
@@ -0,0 +1,18 @@
+package domain.game.ai.ReversiAI.AIs;
+
+import domain.game.ai.ReversiAI.SuperClassesInterfaces.AI;
+
+public class MiniMaxAI extends AI {
+
+    public int getBestMove(long playerWhitePieces, long playerBlackPieces, boolean isWhiteTurn) {
+        return 0;
+    }
+
+    public int getBestMove(boolean[] playerWhitePieces, boolean[] playerBlackPieces, boolean isWhiteTurn) {
+        return 0;
+    }
+
+    public String getAIName() {
+        return "MiniMaxAI";
+    }
+}
