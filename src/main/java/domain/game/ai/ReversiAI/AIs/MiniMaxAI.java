45122fd12a637824b62e95f4c73120f86bbcdd88
Jeffrey-Meijer <jeffreymeijer11@gmail.com>
2023-01-23 16:13:31 +0100

    Fixes errors

--- src/main/java/domain/game/ai/ReversiAI/AIs/MiniMaxAI.java
+++ src/main/java/domain/game/ai/ReversiAI/AIs/MiniMaxAI.java
@@ -9,44 +9,30 @@ import domain.game.ai.ReversiAI.SuperClassesInterfaces.AI;
 public class MiniMaxAI implements AI {
     private final int MAX_DEPTH = 3;
     // isMax = zwarte speler
-    private int calculateMiniMax(int position, boolean isMax, int depth) {
+    private int calculateMiniMax(BoardPosition position, boolean isMax, int depth) {
         // Get state of position here for usage in base case
-//        int evaluation = GreedyEvaluation.evaluate()
-        if (position == 0 || depth == MAX_DEPTH) {
+        int evaluation = GreedyEvaluation.evaluate(position.playerWhitePieces, position.playerBlackPieces, position.isWhiteTurn);
+        if (evaluation == 0 || depth == MAX_DEPTH) {
+            return evaluation;
         }
         if (isMax) {
             int max_score = Integer.MAX_VALUE;
+            int moves[] = MoveFinderFast.findAvailableMoves(position.playerWhitePieces, position.playerBlackPieces, position.isWhiteTurn);
+            for (int move : moves) {
+                int new_position = move;
+//                int score = calculateMiniMax();
+            }
             
         } else {
             int min_score = Integer.MIN_VALUE;
         }
+        return 0;
     }
     @Override
     public int getBestMove(long playerWhitePieces, long playerBlackPieces, boolean isWhiteTurn) {
         int[] moves = MoveFinderFast.findAvailableMoves(playerWhitePieces, playerBlackPieces, isWhiteTurn);
         int max_depth = 3;
-        if position is terminal or max depth reached:
-        return eval(position)
-
-        if player == black:
-        max_score = -infinity
-        for each move:
-        new_position = resolve(position, move)
-        score = minimax(new_position, white)
-        if score > max_score:
-        max_score = score
-        best_move = move
-        return max_score
-
-        if player == white:
-        min_score = infinity
-        for each move:
-        new_position = resolve(position, move)
-        score = minimax(new_position, black)
-        if score < min_score:
-        min_score = score
-        best_move = move
-        return min_score;
+        return 0;
     }
 
 
