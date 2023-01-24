326c26cf1f8c4779895012cb5f588f68c4fd7e4b
Xander <a.j.drent@st.hanze.nl>
2023-01-24 15:40:36 +0100

    eval

--- src/main/java/domain/game/ai/ReversiAI/Heuristics/ParityHeuristic.java
+++ src/main/java/domain/game/ai/ReversiAI/Heuristics/ParityHeuristic.java
@@ -6,6 +6,6 @@ import java.util.Arrays;
 
 public abstract class ParityHeuristic implements Heuristic {
     public static int GetHeuristic(long minPlayerPieces, long maxPlayerPieces) {
-        return 100 * ((Long.bitCount(maxPlayerPieces) - Long.bitCount(minPlayerPieces)) / (Long.bitCount(maxPlayerPieces) + Long.bitCount(minPlayerPieces)));
+        return (64 * (Long.bitCount(maxPlayerPieces) - Long.bitCount(minPlayerPieces)) / (Long.bitCount(maxPlayerPieces) + Long.bitCount(minPlayerPieces)));
     }
 }
