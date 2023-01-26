6ed4b21786925a45dc7384372fc8566d0728340a
Xander <a.j.drent@st.hanze.nl>
2023-01-26 12:02:41 +0100

    LongToBoardIntArray

---
+++ src/main/java/domain/game/ai/ReversiAI/Converters/LongToBoardIntArray.java
@@ -0,0 +1,18 @@
+package domain.game.ai.ReversiAI.Converters;
+
+import static domain.game.ai.ReversiAI.Masks.BitMasks.bitMaskSingleBit;
+
+public class LongToBoardIntArray {
+    public static int[] convert(long playerAPieces, long playerBPieces, int aPiecesInt, int bPiecesInt) {
+        int[] board = new int[64];
+        for (int i = 0; i < 64; i++) {
+            if ((playerAPieces & bitMaskSingleBit[i]) != 0) {
+                board[i] = aPiecesInt;
+            }
+            if ((playerBPieces & bitMaskSingleBit[i]) != 0) {
+                board[i] = bPiecesInt;
+            }
+        }
+        return board;
+    }
+}
