62fa2bbeab68ec27250567f2a9238c20d0ccaa1b
_Slubby_ <pmhuberts@gmail.com>
2023-02-10 11:47:19 +0100

    changes

--- src/main/java/support/ais/Reversi/GreedyAI.java
+++ src/main/java/support/ais/Reversi/GreedyAI.java
@@ -1,7 +1,5 @@
 package support.ais.Reversi;
 
-import support.ais.Reversi.AI;
-
 public class GreedyAI extends AI {
     public int getBestMove(long playerWhitePieces, long playerBlackPieces, boolean isWhiteTurn) {
         return 0;
